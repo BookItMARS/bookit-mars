117d67797cd6e820c87c79bf711e8779bb6c8839
Saibhavithra <saibhavithra@gmail.com>
2024-02-06 14:13:30 -0600

    Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
 import BurnerComponent from './components/BurnerComponent';
 import trainTicket from './assets/trainTicket.png';
+import React from "react";
+import "./styles/App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Navbar from "./components/layout/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  
+} from "react-router-dom";
+import NotFound from "./components/pages/Notfound";
+import BookTicket from "./components/ticket/BookTicket";
+
 
 const App = () => {
 
   return(
     <main>
+      <Router>
+      <div className="App">
       <h1>Welcome to BookIt - MARS</h1>
       <p>I'm a really cool Frontend project</p>
       <img src={trainTicket} alt="look, we imported an asset!" />
+      <Navbar />
       <BurnerComponent />
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About/>} />
+          <Route exact path="/contact" element={<Contact/>} />
+          <Route exact path="/users/add" element={<BookTicket/>} />      
+          
+          <Route element={<NotFound/>} />
+        </Routes>
+
+      </div>
+    </Router>
     </main>
+   
+    
   );
 };
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>Welcome to IRCTC Booking Service</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
